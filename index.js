2fcdfbd57c14c1d1bcf6e997514c5dd3833c8128
Grace Durant <gdurant34@gmail.com>
2022-10-04 10:32:38 -0700

    initial functionality for the form

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ function renderSavedRecipes(recipes) {
 const renderOneRecipeCard = recipe => {
     const recipeDiv = document.createElement('div')
     const recipeName = document.createElement('h3')
-    const recipeDirections = document.createElement('p')
+    const recipeInstructions = document.createElement('p')
     const recipeImage = document.createElement('img')
     const recipeIngredientsList = document.createElement('ul')
     const recipeInstructionsTitle = document.createElement('h4')
@@ -24,7 +24,7 @@ const renderOneRecipeCard = recipe => {
 
     recipeDiv.className = 'one-saved-recipe'
     recipeName.textContent = recipe.name 
-    recipeDirections.textContent = recipe.directions
+    recipeInstructions.textContent = recipe.instructions
     recipeInstructionsTitle.textContent = 'Instructions:'
     recipeIngredientsTitle.textContent = 'Ingredients:'
     recipeImage.src = recipe.image 
@@ -45,9 +45,44 @@ const renderOneRecipeCard = recipe => {
     }
 
 // put the name, image, list of ingredients, and instructions in the card div
-    recipeDiv.append(recipeName, recipeImage, recipeIngredientsTitle, recipeIngredientsList, recipeInstructionsTitle, recipeDirections)
+    recipeDiv.append(recipeName, recipeImage, recipeIngredientsTitle, recipeIngredientsList, recipeInstructionsTitle, recipeInstructions)
 
 // get the section and add the div to it
     const allRecipesDiv = document.getElementById('all-saved-recipes')
     allRecipesDiv.append(recipeDiv)
+}
+
+const form = document.getElementById('new-recipe-form');
+form.addEventListener('submit', addNewRecipe);
+
+function addNewRecipe(e) {
+    e.preventDefault();
+    console.log(e.target['unit-1'].value)
+    const newRecipe = {
+        name: e.target['recipe-name'].value,
+        instructions: e.target['instructions'].value,
+        image: e.target['recipe-image'].value,
+        ingredients: [
+            {name: e.target['ingredient-1'].value, quantity: e.target['qty-1'].value, unit: e.target['unit-1'].value},
+            {name: e.target['ingredient-2'].value, quantity: e.target['qty-2'].value, unit: e.target['unit-2'].value},
+            {name: e.target['ingredient-3'].value, quantity: e.target['qty-3'].value, unit: e.target['unit-3'].value},
+            {name: e.target['ingredient-4'].value, quantity: e.target['qty-4'].value, unit: e.target['unit-4'].value},
+            {name: e.target['ingredient-5'].value, quantity: e.target['qty-5'].value, unit: e.target['unit-5'].value}
+        ]
+    }
+    postNewRecipe(newRecipe);
+
+}
+
+function postNewRecipe(newRecipe) {
+    fetch(url, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(newRecipe)
+    })
+    .then(res => res.json())
+    .then(renderOneRecipeCard(newRecipe))
+    .catch((error) => {console.error('Error:', error)})
 }
